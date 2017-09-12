4ba40fcfe02f031292b7b67e8c7b85505a51f43a
krackWingSam <sangwoo21@askstory.com>
2017-09-12 15:25:31 +0900

    사용법
    테스트를 위한 구문 추가

--- ssparser/test.py
+++ ssparser/test.py
@@ -1,5 +1,12 @@
 import ssparser
+import slsmaker
 
-parser = ssparser.ssparser()
-parser.isDebug = True
-parser.setFilePath('./data_set/naver_crawler.txt')
+# parser = ssparser.ssparser()
+# parser.isDebug = True
+# parser.setFilePath('./data_set/naver_crawler.txt')
+
+maker = slsmaker.slsmaker()
+maker.isDebug = True
+maker.leaningDataCount = 100
+maker.filePath = './output/data.txt'
+maker.writeLeaningData()
